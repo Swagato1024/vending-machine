c6eeb91cd9c973ff5b5e4d0f349e9b280e5c3a64
Swagato Chakraborty <swagatochakraborty@Swagatos-MacBook-Pro.local>
2023-04-11 17:26:32 +0530

    Now vending machine can dispense 10,5,2,1 rupee coins

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,12 +1,27 @@
 
+const denominations = [ 10, 5, 2, 1 ];
+
+const optimumNoOfCoins = {};
+
+const addAll = function (obj) {
+  let sum = 0;
+
+  for (let key in obj) {
+    sum += obj[key];    
+  }
+
+  return sum;
+}
 
 const generateNoOfCoins = function(amount) {
-  let  optimumNoOfCoins = Math.floor(amount / 2);
-  let remainingAmount = amount % 2;
+  let remainingAmount = amount;
 
-  optimumNoOfCoins += remainingAmount;
+  for (let denomination of denominations) {
+    optimumNoOfCoins[denomination] =  Math.floor(remainingAmount / denomination);
+    remainingAmount = remainingAmount % denomination;
+  }
 
-  return optimumNoOfCoins ;
+  return addAll(optimumNoOfCoins);
 }
 
 exports.generateNoOfCoins = generateNoOfCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -14,7 +14,10 @@ const testGenerateCoins = function() {
   testing.assertEquals(vendingMachine.generateNoOfCoins(1), 1, "For amount 1 no Of coins should be 1");
   testing.assertEquals(vendingMachine.generateNoOfCoins(2), 1, "For amount 2 no Of coins should be 1");
   testing.assertEquals(vendingMachine.generateNoOfCoins(4), 2, "For amount 4 no Of coins should be 2");
-  testing.assertEquals(vendingMachine.generateNoOfCoins(5), 3, "For amount 5 no Of coins should be 3");
+  testing.assertEquals(vendingMachine.generateNoOfCoins(5), 1, "For amount 5 no Of coins should be 1");
+  testing.assertEquals(vendingMachine.generateNoOfCoins(10), 1, "For amount 10 no Of coins should be 2");
+  testing.assertEquals(vendingMachine.generateNoOfCoins(9), 3, "For amount 9 no Of coins should be 3");
+  testing.assertEquals(vendingMachine.generateNoOfCoins(13), 3, "For amount 9 no Of coins should be 3");
 }
 
 testGenerateCoins();
