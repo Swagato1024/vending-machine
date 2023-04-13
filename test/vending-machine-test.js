141e8fbb4caa84dc450dfe35af0fe1b2814a0986
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-04-13 12:07:40 +0530

    Refactored test cases

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -10,16 +10,16 @@ const print = function (text) {
 const testGenerateCoins = function() {
   print("testing for given set of dinominations");
 
-  testing.assertEquals(vendingMachine.dispenseCoins(0, [ 1 ]), 0, "For amount zero no Of coins should be zero");
-  testing.assertEquals(vendingMachine.dispenseCoins(1, [ 1, 2 ]), 1, "For amount 1 no Of coins should be 1");
-  testing.assertEquals(vendingMachine.dispenseCoins(2, [ 1, 2]), 1, "For amount 2 no Of coins should be 1");
-  testing.assertEquals(vendingMachine.dispenseCoins(4, [ 1, 2, 5]), 2, "For amount 4 no Of coins should be 2");
-  testing.assertEquals(vendingMachine.dispenseCoins(5, [ 1, 2, 5]), 1, "For amount 5 no Of coins should be 1");
-  testing.assertEquals(vendingMachine.dispenseCoins(10, [ 1, 2, 5, 10]), 1, "For amount 10 and ordered denominations, no Of coins should be 1");
-  testing.assertEquals(vendingMachine.dispenseCoins(9, [ 1, 2, 5, 10]), 3, "For amount 9 and ordered denominations no Of coins should be 3");
-  testing.assertEquals(vendingMachine.dispenseCoins(13, [ 1, 2, 5, 10]), 3, "For amount 13 and ordered denominations no Of coins should be 3");
-  testing.assertEquals(vendingMachine.dispenseCoins(13, [ 1, 4, 7 ]), 4, "For amount 13 and ordered denominations no Of coins should be 4");
-  testing.assertEquals(vendingMachine.dispenseCoins(13, [ 1, 7, 4 ]), 4, "For amount 13 and unordered denominations no Of coins should be 4");
+  testing.assertEquals(vendingMachine.dispenseCoins(0, [ 1 ]), 0, "For amount zero  and denominations [1] no Of coins should be zero");
+  testing.assertEquals(vendingMachine.dispenseCoins(1, [ 1, 2 ]), 1, "For amount 1 and denominations[1,2] no Of coins should be 1");
+  testing.assertEquals(vendingMachine.dispenseCoins(2, [ 1, 2]), 1, "For amount 2 and denominations [1,2] no Of coins should be 1");
+  testing.assertEquals(vendingMachine.dispenseCoins(4, [ 1, 2, 5]), 2, "For amount 4 and denominations [1,2,5] no Of coins should be 2");
+  testing.assertEquals(vendingMachine.dispenseCoins(5, [ 1, 2, 5]), 1, "For amount 5 and denominations [1,2,5,10] no Of coins should be 1");
+  testing.assertEquals(vendingMachine.dispenseCoins(10, [ 1, 2, 5, 10]), 1, "For amount 10 and denominations [1,2,5,10] no Of coins should be 1");
+  testing.assertEquals(vendingMachine.dispenseCoins(9, [ 1, 2, 5, 10]), 3, "For amount 9 and denominations [1,2,5,10] no Of coins should be 3");
+  testing.assertEquals(vendingMachine.dispenseCoins(13, [ 1, 2, 5, 10]), 3, "For amount 13 and denominations [1,2,5,10] no Of coins should be 3");
+  testing.assertEquals(vendingMachine.dispenseCoins(13, [ 1, 4, 7 ]), 4, "For amount 13 and denominations [1,4,7] no Of coins should be 4");
+  testing.assertEquals(vendingMachine.dispenseCoins(13, [ 1, 7, 4 ]), 4, "For amount 13 and denominations [1,7,4] no Of coins should be 4");
 }
 
 testGenerateCoins();
