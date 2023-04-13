64a820eb7897a7bbb2ed333496cc143e132a1435
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-04-14 00:09:30 +0530

    Updated runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 node test/vending-machine-test.js
+node test/utilities-test.js
+node test/sort-test.js
---
+++ test/utilities-test.js
@@ -0,0 +1,36 @@
+const testing = require('../lib/testing.js');
+const utilities = require('../lib/utilities.js');
+
+const assertEquals = testing.assertEquals;
+const sumOf = utilities.sumOf;
+const addAll = utilities.addAll;
+
+const print = function (text) {
+  console.log(testing.yellow((testing.underline(text))));
+}
+
+console.log('hello');
+
+const testSumOf = function () {
+  print('test for sumOf elements');
+
+  assertEquals(sumOf([1]), 1, 'Sum of one element is the number itself');
+  assertEquals(sumOf([1, 2]), 3, 'Sum of two elements');
+  assertEquals(sumOf([1, 2, 4]), 7, 'Sum of more than two elements');
+}
+
+
+const testAddAll = function () {
+  print('testAddAll values of an object');
+
+  assertEquals(addAll({'a': 1}), 1, 'Sum of values of an object with one key is the value itself');
+  assertEquals(addAll({'a': 1, 'b': 2}), 3, 'Sum of values of an object with any no of keys');
+}
+
+
+const runTest = function () {
+  testSumOf();
+  testAddAll();
+}
+
+runTest();
